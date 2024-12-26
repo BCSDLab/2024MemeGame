4b6766365e4fe19c96dc516449681cda080f5ee5
Lee Seung Been <sudangi143@gmail.com>
2024-12-26 18:01:58 +0900

    bugfix / saveSystem

--- Assets/Scripts/GameOverBoundary.cs
+++ Assets/Scripts/GameOverBoundary.cs
@@ -6,12 +6,20 @@ public class GameOverBoundary : MonoBehaviour
 {
     public static UnityEvent OnGameOver = new UnityEvent();
 
+    [SerializeField] private GameManager gameManager;
+
+    public void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
     public void OnTriggerStay2D(Collider2D collision)
     {
         ObjectMerge obm = collision.gameObject.GetComponent<ObjectMerge>();
 
         if(obm != null && obm.isCollided)
         {
+            gameManager.SaveMaxScore();
             OnGameOver?.Invoke();
         }
     }
